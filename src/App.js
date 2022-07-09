fd2e81315b64f2d9e8ca06be242f34a283647211
trihlm <hlmtri92@gmail.com>
2022-07-09 12:02:34 +0700

    panel, no effect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form class='border border-dark container form-body rounded text-center p-0'>
+      <div class="border-bottom border-dark py-3 bg-out">
+        <h3 class="mb-0" style={{color: '#FFEDD4'}}>Please enter the secondary password.</h3>
+      </div>
+      <div class="form-group px-2 pt-5 bd-body">
+        <input type="text" name="name" id="name" class="form-control w-100"/>
+        <div class="mt-2 pb-5 btn-item">
+          <button class="btn btn-secondary">8</button>
+          <button class="btn btn-secondary">9</button>
+          <button class="btn btn-secondary">4</button>
+          <button class="btn btn-secondary">0</button>
+          <button class="btn btn-secondary">5</button>
+          <button class="btn btn-secondary">left</button>
+          <button class="btn btn-secondary">3</button>
+          <button class="btn btn-secondary">1</button>
+          <button class="btn btn-secondary">2</button>
+          <button class="btn btn-secondary">7</button>
+          <button class="btn btn-secondary">6</button>
+          <button class="btn btn-secondary">C</button>
+        </div>
+      </div>
+      <div class="border-top border-dark bg-out">
+        <h5 class="py-3" style={{color: '#D05D48'}}>* 2nd passwords must be at least 6 characters long.</h5>
+        <div class="row justify-content-around px-2 pb-3">
+          <button class="btn btn-secondary col-5">Confirm</button>
+          <button class="btn btn-secondary col-5">Cancel</button>
+        </div>
+      </div>
+    </form>
   );
 }
 
