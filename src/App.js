4c915a5cbef80f7052813d0aa007a1a7ea843161
fj5701gj <fj5701gj@fujitsu.com>
2022-07-12 16:22:17 +0700

    reformatted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import {shuffle} from './utils/NumberUtils.ts'
+import { shuffle } from './utils/NumberUtils.ts'
 
 function App() {
   let values = shuffle([...Array(10).keys()]);
@@ -10,16 +10,18 @@ function App() {
   return (
     <form className='border border-dark container form-body rounded text-center p-0'>
       <div className="border-bottom border-dark py-3 bg-out">
-        <h3 className="mb-0" style={{color: '#FFEDD4'}}>Please enter the secondary password.</h3>
+        <h3 className="mb-0" style={{ color: '#FFEDD4' }}>Please enter the secondary password.</h3>
       </div>
       <div className="form-group px-2 pt-5 bd-body">
-        <input type="password" name="name" id="name" className="form-control w-100"/>
+        <input type="password" name="name" id="name" className="form-control w-100" />
         <div className=" num-grp" >
           <div className="mt-2 pb-5 btn-item col-10">
             {
-              values.map(element => { return (
-                <button className="btn btn-secondary" key={element}>{element}</button>
-              )})
+              values.map(element => {
+                return (
+                  <button className="btn btn-secondary" key={element}>{element}</button>
+                )
+              })
             }
           </div>
           <div className="mt-2 pb-5 btn-item col-2">
@@ -29,7 +31,7 @@ function App() {
         </div>
       </div>
       <div className="border-top border-dark bg-out">
-        <h5 className="py-3" style={{color: '#D05D48'}}>* 2nd passwords must be at least 6 characters long.</h5>
+        <h5 className="py-3" style={{ color: '#D05D48' }}>* 2nd passwords must be at least 6 characters long.</h5>
         <div className="row justify-content-around px-2 pb-3">
           <button className="btn btn-secondary col-5">Confirm</button>
           <button className="btn btn-secondary col-5">Cancel</button>
